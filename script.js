51d34450f12155a2bf54b8098f90907d12b1515e
SimYuanYee <s10223621@connect.np.edu.sg>
2021-11-30 11:17:03 +0800

    l

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function display_message(){
     var today=new Date();
     document.getElementById("message").innerHTML = "Thank You!";
-    var date=today.getDate();
+    console.log(today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds());
 }
 
 function display(){
