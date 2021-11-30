5f5785e1f31e5053c20614b075d58a0b37cb9893
SimYuanYee <s10223621@connect.np.edu.sg>
2021-11-30 12:01:25 +0800

    l

--- script.js
+++ script.js
@@ -2,13 +2,11 @@ function display_message(){
     document.getElementById("message").innerHTML = "Thank You!";
 }
 
-function display_time(){
-    var today=new Date();
-    var time = today.getDate();
-    document.write(time);
-
-}
 
 function display(){
     document.getElementById('moreinfo1').innerHTML="For PolyTechnic,I am currently Year 2. I have learnt CyberSecurity,Data Science,Python,Computing Maths and Design Skills"
+}
+
+function display2(){
+    document.getElementById('moreinfo2').innerHTML="Other set of Skills im decent at is CyberSecurity and Anaylsing Data"
 }
